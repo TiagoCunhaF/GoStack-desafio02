5a5212177d3b88f3c9b1d8a102ccf1f5d41d513d
Tiago Cunha <tiagocunha@Tiagos-Air.mygateway>
2020-06-13 15:03:01 -0300

    feat: implement challenge 02 routes to passe for all tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,74 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return res.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+
+  const repositoryIndex = repositories.findIndex((item) => item.id === id);
+
+  if (repositoryIndex === -1)
+    return res
+      .status(400)
+      .json({ error: `Repository with id=${id} not exists` });
+
+  repositories[repositoryIndex] = {
+    ...repositories[repositoryIndex],
+    title,
+    url,
+    techs,
+  };
+  return res.json(repositories[repositoryIndex]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const repositoryIndex = repositories.findIndex((item) => item.id == id);
+
+  if (repositoryIndex === -1)
+    return res
+      .status(400)
+      .json({ error: `Repository with id=${id} not exists` });
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex((item) => item.id == id);
+
+  if (repositoryIndex === -1)
+    return res
+      .status(400)
+      .json({ error: `Repository with id=${id} not exists` });
+
+  repositories[repositoryIndex].likes = repositories[repositoryIndex].likes + 1;
+
+  //return res.status(200).json(repositories[repositoryIndex]);
+  return res.status(200).json({ likes: repositories[repositoryIndex].likes });
 });
 
 module.exports = app;
